6048d644a9797eefb3714dbe153491e7d9873d1e
Zach Thygesen <zsthygesen@gmail.com>
2022-06-09 18:46:19 -0500

    Finish exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let indexOldest = 0;
+    let oldest = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].yearOfDeath - array[i].yearOfBirth > oldest) {
+            oldest = array[i].yearOfDeath - array[i].yearOfBirth;
+            indexOldest = i;
+        }
+    }
+    return array[indexOldest];
 
 };
 
